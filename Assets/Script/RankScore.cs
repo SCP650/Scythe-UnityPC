684ca17c708443ae566d527204c6f8a1446d1854
Sebastian Yang <yukaiy@andrew.cmu.edu>
2021-01-31 10:59:17 -0500

    rank socre is up but return to manul is not

---
+++ Assets/Script/RankScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RankScore : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI currentScore;
+    [SerializeField] TextMeshProUGUI bestScore;
+
+    private void Start()
+    {
+        int current = PlayerPrefs.GetInt("Current");
+        int best = PlayerPrefs.GetInt("Best", current);
+        if (current > best)
+        {
+            best = current;
+            PlayerPrefs.SetInt("Best", best);
+        }
+        currentScore.text = "Your Score: " + current;
+        bestScore.text = "Best Score: " + best;
+
+    }
+  
+   
+   
+
+}
